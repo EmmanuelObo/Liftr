502b25fea979ea8063ed7181ace3015e464fb90d
cohenta2 <cohenta2@mymail.vcu.edu>
2017-09-16 16:34:37 -0400

    Added base Template

--- liftr/liftr/settings.py
+++ liftr/liftr/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'liftr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./liftr/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
