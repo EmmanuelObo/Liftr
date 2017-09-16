623ad1da56841867e234ca77691d6b9386277629
Emmanuel Obogbaimhe <eobogbai@buffalo.edu>
2017-09-16 15:12:13 -0400

    User Preference Model Created

--- liftr/person/models.py
+++ liftr/person/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+gender =(
+('M','Male'),
+( 'F', 'Female')
+)
+
+class Person(models.Model):
+    user = models.OnetoOneField(User, on_delete=models.CASCADE())
+    gender = models.CharField(choices=gender)
+    age = models.IntegerField()
+    location = models.CharField()
+    bio = models.TextField()
