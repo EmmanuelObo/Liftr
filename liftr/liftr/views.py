a579baed7774d265447803c1ee8966a26a0b0518
cohenta2 <cohenta2@mymail.vcu.edu>
2017-09-17 11:42:48 -0400

    Added randomized geolocation

--- liftr/liftr/views.py
+++ liftr/liftr/views.py
@@ -4,6 +4,7 @@ from geopy.distance import vincenty
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from random import randint
 
 def home(request):
     if request.user.is_authenticated():
@@ -30,25 +31,22 @@ def user_login(request):
     else:
         return render(request, 'sub_templates/login.html')
 
+def edit(request):
+    return render(request, 'sub_templates/edit.html')
+
 def user_logout(request):
     if request.user.is_authenticated():
         logout(request)
         return HttpResponseRedirect('/home')
 
 def coordinates(request):
-    if request.method == 'POST':
-        latitude = request.POST.get('latitude')
-        longitude = request.POST.get('longitude')
-        current_user = User.objects.get(username='Test')
-        other_user = User.objects.get(username='TEST2')
+    if request.method == 'GET':
+        request.user.person.latitude = randint(30,50)
+        request.user.person.longitude = randint(-110, -70)
         geolocator = Nominatim()
-        other_user.person.latitude = 38
-        other_user.person.longitude = -77
-        current_user.person.latitude = float(latitude)
-        current_user.person.longitude = float(longitude)
-        current_location = (current_user.person.latitude, current_user.person.longitude)
-        other_location = (other_user.person.latitude, other_user.person.longitude)
-        print(vincenty(current_location, other_location).miles)
-        print(current_user.person.dict())
-        print(other_user.person.dict())
-        return render(request, "base.html", {})
+        current_location = ("{}, {}".format(request.user.person.latitude, request.user.person.longitude))
+        location = geolocator.reverse(current_location)
+        print(location.address)
+        return render(request, "sub_templates/edit.html", {'data' : location.address})
+    else:
+        return render(request,  "sub_templates/workout.html")
