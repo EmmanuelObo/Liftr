7c5f6f689649f4c42c56c69f88c6c529c1c773c4
cohenta2 <cohenta2@mymail.vcu.edu>
2017-09-17 03:39:36 -0400

    Added distance calculation

--- liftr/liftr/views.py
+++ liftr/liftr/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from geopy.geocoders import Nominatim
+from geopy.distance import vincenty
+
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, "base.html", {})
@@ -7,12 +10,18 @@ def home(request):
 
 def coordinates(request):
     if request.method == 'POST':
-        print("hello")
         latitude = request.POST.get('latitude')
         longitude = request.POST.get('longitude')
-        print("latitude{} longitude{}".format(latitude,longitude))
+        current_user = User.objects.get(username='Test')
+        other_user = User.objects.get(username='TEST2')
         geolocator = Nominatim()
-
-        location = geolocator.reverse("{}, {}".format(latitude,longitude))
-        print(location.address)
+        other_user.person.latitude = 38
+        other_user.person.longitude = -77
+        current_user.person.latitude = float(latitude)
+        current_user.person.longitude = float(longitude)
+        current_location = (current_user.person.latitude, current_user.person.longitude)
+        other_location = (other_user.person.latitude, other_user.person.longitude)
+        print(vincenty(current_location, other_location).miles)
+        print(current_user.person.dict())
+        print(other_user.person.dict())
         return render(request, "base.html", {})
