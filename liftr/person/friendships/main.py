8b43fdb04b46a36462f97e7b82538c095fe789d1
Emmanuel Obogbaimhe <eobogbai@buffalo.edu>
2017-09-17 01:15:27 -0400

    Selection Pool [Work in Progress]

--- liftr/person/friendships/main.py
+++ liftr/person/friendships/main.py
@@ -4,12 +4,10 @@ def send_request(curr_user, recipient, msg="{} wants to partner with you!"):
     msg.format(curr_user.username)
     Friend.objects.add_friend(curr_user,recipient,msg)
 
- def accept_request():
-    FriendshipRequest.objects.filter(to_user=jon)
+def accept_request(curr_user):
+     FriendshipRequest.objects.get(to_user=curr_user).accept()
 
-
-
-
-# def reject_request():
+def reject_request(curr_user):
+     FriendshipRequest.objects.get(to_user=curr_user).reject()
 
 
--- liftr/preference/models.py
+++ liftr/preference/models.py
@@ -6,11 +6,11 @@ from person.models import Person
 class Preference(models.Model):
     availability = []
     distance = models.IntegerField()
-    user = models.ForeignKey(Person, on_delete=models.CASCADE, null=True)
+    user = models.OneToOneField(Person, on_delete=models.CASCADE, null=True)
 
 
     def set_availability(self, days):
         self.availability = days
 
-
-
+    def __str__(self):
+        return "{}'s Preference".format(self.user.user.username)
