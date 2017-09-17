5a50014b434f28f7c5b1a51403d67274c4909b66
cohenta2 <cohenta2@mymail.vcu.edu>
2017-09-17 00:22:40 -0400

    message

--- liftr/liftr/views.py
+++ liftr/liftr/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from geopy.geocoders import Nominatim
 
 def home(request):
     return render(request, "base.html", {})
@@ -8,5 +9,7 @@ def coordinates(request):
     if request.method == 'POST':
         latitude = request.POST.get('latitude')
         longitude = request.POST.get('longitude')
+        geolocator = Nominatim()
+        
         print(latitude, longitude)
         return render(request, "base.html", {})
