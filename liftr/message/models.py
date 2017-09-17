328bdcc4417c25198cbf0ab9f285dcecf93c98e8
Emmanuel Obogbaimhe <eobogbai@buffalo.edu>
2017-09-17 06:14:43 -0400

    Messages

--- liftr/message/models.py
+++ liftr/message/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class MessageManager(models.Manager):
+    def history(self, curr_user, chat_buddy):
+        messages = Message.objects.get(from_user=curr_user,to_user=chat_buddy)
+
+        return (message.message_details+"\n" for message in messages)
+
+
+class Message(models.Model):
+    from_user = models.OneToOneField(User, related_name="sender")
+    to_user = models.OneToOneField(User, related_name="receiver")
+    sent_at = models.DateField(default=timezone.now)
+    message = models.TextField()
+
+    @property
+    def message_details(self):
+        return "{} \tSent by: {} (@ {})".format(self.message, self.from_user, self.sent_at)
